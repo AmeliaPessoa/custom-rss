ffacf81a706c27524c1b226767cbd288436bb617
jvalvesdepaiva76 <jv.alvesdepaiva@gmail.com>
2024-06-27 21:48:58 -0300

    selenium+chrome-actor

--- crawler/db/init.sql
+++ crawler/db/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE website (
     id SERIAL PRIMARY KEY,
-    link TEXT NOT NULL UNIQUE,
+    link TEXT NOT NULL,
     title VARCHAR(500) NOT NULL,
-    description TEXT NOT NULL,
-    execution_timestamp TIMESTAMP NOT NULL  -- Combina data e hora de execução do crawler
+    description TEXT,
+    inserted_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE articles (
@@ -11,10 +11,11 @@ CREATE TABLE articles (
     website_id INT NOT NULL,
     link TEXT NOT NULL UNIQUE,
     title VARCHAR(500) NOT NULL,
-    publication_date DATE,
+    publication_date TIMESTAMP,
     author VARCHAR(100),
     description TEXT,
     categories VARCHAR(500),
+    inserted_at TIMESTAMP NOT NULL,
     FOREIGN KEY (website_id) REFERENCES website(id) ON DELETE CASCADE
 );
 
--- crawler/src/main.py
+++ crawler/src/main.py
@@ -1,81 +1,109 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from apify import Actor
 import datetime
 import re
+import psycopg2
 import requests
 from bs4 import BeautifulSoup
-from apify_client import ApifyClient
-import psycopg2
-from apify import Actor
 
 class CrawlerDaCoruna:
     def __init__(self, config):
         self.config = config
-        self.client = ApifyClient(token=self.config['apify']['api_token'])
+        self.driver = self.init_driver()
 
-    def extract_title(self, markdown_content):
-        match = re.search(r'::(.*?)\[(.*?)\]', markdown_content, re.DOTALL)
-        return match.group(2).strip() if match else ""
+    def init_driver(self):
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        return webdriver.Chrome(options=chrome_options)
 
-    def extract_description(self, markdown_content):
-        match = re.search(r'::(.*?)\[(.*?)\]', markdown_content, re.DOTALL)
-        description = match.group(1).strip() if match else ""
-
-        if description.startswith("Ficha Nova\n\n##"):
-            description = description[len("Ficha Nova\n\n##"):]
-        elif description.startswith("Ficha Nova\n\n"):
-            description = description[len("Ficha Nova\n\n"):]
-
-        description = description.replace("\n\n", ". ").replace("\n", ". ")
-        description = description.strip()
-        return description
+    def extract_title_and_description(self):
+        response = requests.get(self.config['startUrls'][0]['url'])
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            title = soup.title.string
+            description = soup.find("meta", attrs={"name": "description"})["content"]
+            return title, description
+        return None, None
 
     def extract_date_and_time(self, loaded_time):
-        return datetime.datetime.strptime(loaded_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+        return datetime.datetime.strptime(loaded_time, "%Y-%m-%dT%H:%M:%S")
 
-    def extract_publication_date(self, url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            publication_date_elem = soup.find(class_=self.config['apify']['publication_date_class'])
-            if publication_date_elem:
-                publication_date = publication_date_elem.get_text().strip()
-                return self.transform_publication_date(publication_date)
+    def transform_publication_date(self, date_str, include_year=True):
+        months = {
+            "xaneiro": "01", "febreiro": "02", "marzo": "03", "abril": "04",
+            "maio": "05", "xuño": "06", "xullo": "07", "agosto": "08",
+            "setembro": "09", "outubro": "10", "novembro": "11", "decembro": "12"
+        }
+        if include_year:
+            match = re.search(r"(\d{2}) de (.*?) de (\d{4})", date_str)
+            if match:
+                day, month, year = match.groups()
+                month = months[month.lower()]
+                return f"{year}-{month}-{day}"
+        else:
+            match = re.search(r"(\d{2}) de (.*?)$", date_str)
+            if match:
+                day, month = match.groups()
+                month = months[month.lower()]
+                year = datetime.datetime.now().year
+                return f"{year}-{month}-{day}"
         return None
 
-    def transform_publication_date(self, date_str):
-        months = {
-            "Xaneiro": "01",
-            "Febreiro": "02",
-            "Marzo": "03",
-            "Abril": "04",
-            "Maio": "05",
-            "Xuño": "06",
-            "Xullo": "07",
-            "Agosto": "08",
-            "Setembro": "09",
-            "Outubro": "10",
-            "Novembro": "11",
-            "Decembro": "12"
-        }
-        day, month, year = re.search(r"(\d{2}) de (.*?) de (\d{4})", date_str).groups()
-        month = months[month]
-        return f"{year}-{month}-{day}"
+    def extract_articles(self):
+        articles = []
+        page_number = 1
+        yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
-    def extract_category(self, url):
-        response = requests.get(url)
+        while True:
+            response = requests.get(f"{self.config['startUrls'][0]['url']}&ccm_paging_p={page_number}")
+            if response.status_code != 200:
+                break
+            soup = BeautifulSoup(response.text, "html.parser")
+            article_elements = soup.select(".c-new--item.u-flex")
+
+            for article_elem in article_elements:
+                try:
+                    date_elem = article_elem.select_one(".c-new--item__date").text.strip()
+                    publication_date = self.transform_publication_date(date_elem, include_year=False)
+
+                    if publication_date < yesterday:
+                        return articles
+
+                    if publication_date == yesterday:
+                        article_link = article_elem.find("a")["href"]
+                        articles.append(self.extract_article_details(article_link))
+                except Exception as e:
+                    Actor.log.exception(f"Error processing article element: {e}")
+
+            page_number += 1
+
+    def extract_article_details(self, url):
+        full_url = self.config['apify']['base_url'] + url
+        response = requests.get(full_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
-            category = ", ".join([a.get_text().strip() for a in soup.select(self.config['apify']['category_selector'])])
-            return category
-        return ""
+            title = soup.select_one(".c-article__detail_title").text.strip()
+            publication_date_elem = soup.select_one(".c-article__detail_date")
+            publication_date = publication_date_elem["datetime"] if publication_date_elem else None
+            description = soup.select_one(".c-article__detail_summary").text.strip()
+            categories = ", ".join([cat.text for cat in soup.select(".c-article__left span a")])
+            images_and_videos = self.extract_images_and_videos(full_url)
+            inserted_at = datetime.datetime.now()
 
-    def extract_author(self, url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, "html.parser")
-            author_elem = soup.find(class_="author_class")  # Ajuste esta linha conforme necessário
-            if author_elem:
-                return author_elem.get_text().strip()
-        return ""
+            return {
+                'link': full_url,
+                'title': title,
+                'publication_date': publication_date,
+                'author': None,
+                'description': description,
+                'categories': categories,
+                'images_and_videos': images_and_videos,
+                'inserted_at': inserted_at
+            }
+        return {}
 
     def extract_images_and_videos(self, url):
         response = requests.get(url)
@@ -110,55 +138,22 @@ class CrawlerDaCoruna:
 
     def run_crawler(self):
         Actor.log.info('Starting the crawler...')
-        actor_input = {
-            'startUrls': self.config['startUrls'],
-            'includeUrlGlobs': self.config['includeUrlGlobs'],
-            'excludeUrlGlobs': self.config['excludeUrlGlobs'],
-            'requestTimeoutSecs': self.config['requestTimeoutSecs'],
-            'maxCrawlPages': self.config['maxCrawlPages']
+        title, description = self.extract_title_and_description()
+        inserted_at = datetime.datetime.now()
+
+        website_data = {
+            'link': self.config['startUrls'][0]['url'],
+            'title': title,
+            'description': description,
+            'inserted_at': inserted_at
         }
 
-        run = self.client.actor('apify/website-content-crawler').call(run_input=actor_input)
-        Actor.log.info('Crawler run finished.')
+        articles = self.extract_articles()
 
-        results = [item for item in self.client.dataset(run['defaultDatasetId']).iterate_items()]
-
-        processed_results = []
-        for result in results:
-            website_data = {
-                'link': result.get('url', ""),
-                'title': self.extract_title(result.get('markdown', "")),
-                'description': self.extract_description(result.get('markdown', "")),
-                'execution_timestamp': self.extract_date_and_time(result['crawl']['loadedTime'])
-            }
-
-            article_link = website_data['link']
-            publication_date = self.extract_publication_date(article_link)
-
-            yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-            if publication_date != yesterday.strftime("%Y-%m-%d"):
-                continue
-
-            category = self.extract_category(article_link)
-            author = self.extract_author(article_link)
-            images_and_videos = self.extract_images_and_videos(article_link)
-
-            article = {
-                'link': article_link,
-                'title': website_data['title'],
-                'publication_date': publication_date,
-                'author': author,
-                'description': website_data['description'],
-                'categories': category,
-                'images_and_videos': images_and_videos
-            }
-
-            processed_result = {
-                'website_data': website_data,
-                'article': article
-            }
-
-            processed_results.append(processed_result)
+        processed_results = [{
+            'website_data': website_data,
+            'article': article
+        } for article in articles]
 
         Actor.log.info(f'Processed {len(processed_results)} results.')
         self.insert_results_to_postgresql(processed_results)
@@ -175,34 +170,39 @@ class CrawlerDaCoruna:
         cursor = conn.cursor()
         Actor.log.info('Connected to PostgreSQL.')
 
-        for result in results:
-            website_data = result['website_data']
-            article = result['article']
+        website_data = results[0]['website_data']
+        cursor.execute("""
+            INSERT INTO website (link, title, description, inserted_at)
+            VALUES (%s, %s, %s, %s)
+            RETURNING id;
+        """, (
+            website_data['link'],
+            website_data['title'],
+            website_data['description'],
+            website_data['inserted_at']
+        ))
 
+        website_id = cursor.fetchone()[0]
+
+        for result in results:
+            article = result['article']
             cursor.execute("""
-                WITH website AS (
-                    INSERT INTO website (link, title, description, execution_timestamp)
-                    VALUES (%s, %s, %s, %s)
-                    RETURNING id
-                ),
-                article_insert AS (
-                    INSERT INTO articles (website_id, link, title, publication_date, author, description, categories)
-                    SELECT id, %s, %s, %s, %s, %s, %s FROM website
+                WITH article_insert AS (
+                    INSERT INTO articles (website_id, link, title, publication_date, author, description, categories, inserted_at)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                     RETURNING id
                 )
                 INSERT INTO media (article_id, link, title, description)
                 SELECT id, %s, %s, %s FROM article_insert;
             """, (
-                website_data['link'],
-                website_data['title'],
-                website_data['description'],
-                website_data['execution_timestamp'],
+                website_id,
                 article['link'],
                 article['title'],
                 article['publication_date'],
                 article['author'],
                 article['description'],
                 article['categories'],
+                article['inserted_at'],
                 article['images_and_videos'][0]['link'] if article['images_and_videos'] else None,
                 article['images_and_videos'][0]['title'] if article['images_and_videos'] else None,
                 article['images_and_videos'][0]['description'] if article['images_and_videos'] else None
@@ -235,3 +235,7 @@ async def main():
         crawler.run_crawler()
 
         Actor.log.info('CrawlerDaCoruna finished.')
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
